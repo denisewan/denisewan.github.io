7b26e705deddcf65dcc5aeaf3db65461b937e309
Denise <denise.wan@uwcsea.edu.sg>
2018-10-17 11:14:08 +0800

    amend

--- Part-3-Javascript.js
+++ Part-3-Javascript.js
@@ -6,8 +6,8 @@ function conDition(){
 	num1 = prompt("Please enter a number from 1 to 10"); 	
 
 	if(num1 >= 1 && num1 <= 10){
-		
-    	outNum(); 
+
+        document.write("Your number (" + number + ") is matches requirements", "");
 
 	}else if(num1 < 1){
 		alert("The number is too low for this function."); 	
