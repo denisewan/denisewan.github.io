3efa7c5b0dfa8376fe37192a90b360bec72c52e2
Denise <denise.wan@uwcsea.edu.sg>
2018-10-17 10:48:03 +0800

    amend

--- Part-3-Javascript.js
+++ Part-3-Javascript.js
@@ -2,6 +2,6 @@
 var num = Math.ceil(Math.random() * 10);
 var gnum = prompt('Guess the number between 1 and 10 inclusive');
 if (gnum == num)
-  alert('Matched');
+  alert('You read my memory');
  else
   alert('Not matched, the number was ' + num);
