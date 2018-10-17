e46f358eb2a2ab90bdc04932ee152c64b818ea76
Denise <denise.wan@uwcsea.edu.sg>
2018-10-17 10:54:37 +0800

    amend

--- Part-3-Javascript.js
+++ Part-3-Javascript.js
@@ -1,6 +1,38 @@
-var num = Math.ceil(Math.random() * 10);
-var gnum = prompt('Guess the number between 1 and 10 inclusive');
-if (gnum == num)
-  alert('You read my memory');
- else
-  alert('Not matched, the number was ' + num);
+var num1;
+conDition(); 
+
+function conDition(){
+	
+	num1 = prompt("Please enter a number from 1 to 10"); 	
+
+	if(num1 >= 1 && num1 <= 10){
+		
+    	outNum(); 
+
+	}else if(num1 < 1){
+		alert("The number is too low for this function."); 	
+		num1 = 0; 
+		conDition();	
+
+	}else if(num1 > 10){
+		alert("The number is too high for this function.");
+		num1 = 0; 
+		conDition();
+		
+	}else{
+		alert("That is not a number!"); 
+		num1 = 0;
+		conDition();
+	}
+}
+
+function outNum(){
+	do
+	{
+    	document.write(num1 + "<br>");
+    	num1++;
+	}while (num1 <= 300);
+
+	document.write("############################<br>");	
+	return true; 
+}
