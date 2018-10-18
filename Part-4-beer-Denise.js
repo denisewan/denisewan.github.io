6197bcc571a06d9ddc893cfdaeae52653d551c66
Denise <denise.wan@uwcsea.edu.sg>
2018-10-18 14:57:45 +0800

    amend

--- Part-4-beer-Denise.js
+++ Part-4-beer-Denise.js
@@ -1,9 +1,20 @@
-var counter=0,
-    $bellNotificator = $('#header_bell_notificator'),
-    $bellNotificatorImg = $bellNotificator.find('img');
-
-$( "#kick_button" ).click(function() {
-    counter++;
-    $bellNotificator.attr('data-counter', counter);
-    $bellNotificatorImg.removeClass("show_notif");
-});
+var bottles;
+for (counter = 99; counter >= 1; counter = counter - 1) 
+{
+    if (counter == 1) {
+        bottles = 'bottle';
+    } else {
+        bottles = 'bottles';
+    }
+    console.log(counter+" "+bottles+" of beer on the wall.");
+    if (counter < 99) {
+        console.log("");
+        console.log(counter+" "+bottles+" of beer on the wall.");
+    }
+    console.log(counter+" "+bottles+" of beer.");
+    console.log("Take one down.");
+    console.log("Pass it around.");
+    if (counter == 1) {
+        console.log("No bottles of beer on the wall.");
+    }
+}
