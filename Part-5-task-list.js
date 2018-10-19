79d5f7c8ea9f6418bc471e79c1ef6aeda79c4a9d
Denise <denise.wan@uwcsea.edu.sg>
2018-10-19 15:38:09 +0800

    amend

--- Part-5-task-list.js
+++ Part-5-task-list.js
@@ -2,8 +2,8 @@
 var mkList = function (taskName, dueDate, priority){
     //create <li></li>
     var liElem = document.createElement('li');
-    // >li> a task - due date  </li>
-    liElem.textContent = '${taskName} - ${dueDate}';
+    // <li> a task - due date  </li>
+    liElem.textContent = `${taskName} - ${dueDate}`;
     // <li class="list-group"> a task - due date </li>
     liElem.classList.add('list-group-item');
     return (liElem)
