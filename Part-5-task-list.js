a79115eecd8d02cdcbf4075df9d0cbbe2ac31506
Denise <denise.wan@uwcsea.edu.sg>
2018-10-19 14:53:02 +0800

    amend

---
+++ Part-5-task-list.js
@@ -0,0 +1,25 @@
+//select button
+var addBtn = document.querySelector('#addBtn')
+addBtn.addEventListener(
+    'click',
+    function(){
+        var taskElem = document.querySelector('#task')
+        console.log ('task: ', taskElem.value)
+    }
+)
+
+addBtn.addEventListener(
+    'click',
+    function(){
+        var duedateElem = document.querySelector('#due-date')
+        console.log ('Due date: ', duedateElem.value)
+    }
+)
+
+addBtn.addEventListener(
+    'click',
+    function(){
+        var duedateElem = document.querySelector('#priority')
+        console.log ('Priority: ', priorityElem.value)
+    }
+)
