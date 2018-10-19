fd46a51ca403ef48e3a2970bdaf4811708152637
Denise <denise.wan@uwcsea.edu.sg>
2018-10-19 15:27:31 +0800

    amend

--- Part-5-task-list.js
+++ Part-5-task-list.js
@@ -1,25 +1,25 @@
 //select button
+var mkList = function (taskName, dueDate, priority){
+    //create <li></li>
+    var liElem = document.createElement('li');
+    // >li> a task - due date  </li>
+    liElem.textContent = '${taskName} - ${dueDate}';
+    // <li class="list-group"> a task - due date </li>
+    liElem.classList.add('list-group-item');
+    return (liElem)
+}
+
+
 var addBtn = document.querySelector('#addBtn')
 addBtn.addEventListener(
     'click',
     function(){
-        var taskElem = document.querySelector('#task')
-        console.log ('task: ', taskElem.value)
-    }
-)
+        var taskElem = document.querySelector('#task');
+        var duedateElem = document.querySelector('#due-date');
+        var priorityElem = document.querySelector('#priority');
+        var todoElem = mkList(taskElem.value, duedateElem.value, priorityElem.value);
+        var todoListElem = document.querySelector('#todo-list');
+        todoListElem.appendChild(todoElem);
 
-addBtn.addEventListener(
-    'click',
-    function(){
-        var duedateElem = document.querySelector('#due-date')
-        console.log ('Due date: ', duedateElem.value)
-    }
-)
-
-addBtn.addEventListener(
-    'click',
-    function(){
-        var priorityElem = document.querySelector('#priority')
-        console.log ('Priority: ', priorityElem.value)
     }
 )
